e3277ea3fe28ca02edb2bd3358caf752d3e828b2
Байназар Сапарбек Даулетулы <@corp.itstep>
2019-03-19 18:07:40 +0600

    adding files

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type Something = typeof(String);
+            MethodInfo method = Something.GetMethod("Substring", new Type[] { typeof(int), typeof(int) });
+            Object text = method.Invoke("there was a Sapar", new object[] { 11, 6 });
+            Console.WriteLine($"{text}");
+
+            Type Lists = typeof(List<>);
+            foreach(var something in Lists.GetMembers())
+            {
+                if(something is ConstructorInfo)
+                {
+                    Console.WriteLine($"{something}");
+                }
+            }
+
+
+
+        }
+
+        
+
+
+    }
+
+
+
+
+}
